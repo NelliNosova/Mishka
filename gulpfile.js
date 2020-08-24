8a9d931be6d1092f1f47bcde4d6f0853c3a22997
Nelli Nosova <nellinosovapl@gmail.com>
2020-08-24 12:29:41 +0200

    htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
 const del = require("del");
+const htmlmin = require('gulp-htmlmin');
 
 // Styles
 
@@ -108,6 +109,7 @@ exports.clean = clean;
 
 const html = () => {
   return gulp.src("source/*.html")
+  .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(gulp.dest("build"))
   .pipe(sync.stream());
 }
