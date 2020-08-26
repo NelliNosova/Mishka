52e70290715fee411f41bcbc89122de4faf2ce24
Nelli Nosova <nellinosovapl@gmail.com>
2020-08-26 18:07:49 +0200

    js для модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,21 @@
 (function () {
-  var order = document.querySelector(".js-order");
+  var order = document.querySelectorAll(".js-order");
   var modalOrder = document.querySelector(".js-modal");
 
-  order.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalOrder.classList.remove("none");
+  order.forEach(function(open) {
+    open.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      modalOrder.classList.remove("none");
+    })
   });
 
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-    if (!modalOrder.classList.contains("none")) {
-      evt.preventDefault();
-      modalOrder.classList.add("none");
+      if (!modalOrder.classList.contains("none")) {
+        evt.preventDefault();
+        modalOrder.classList.add("none");
+      }
     }
-  }
-}) ();
+  });
+
+})();
